6bd9d0940a138dced12643953c1f16147a801f9d
Manish <75516081+Manish263@users.noreply.github.com>
2023-02-18 21:37:46 +0530

    Create  Invert Binary Tree.cpp

---
+++ 18-02-2023/Invert Binary Tree.cpp
@@ -0,0 +1,12 @@
+ Invert Binary Tree
+ TreeNode* invertTree(TreeNode* root) {
+         if(root == NULL){
+            return NULL;
+        }
+        invertTree(root->left);
+        invertTree(root->right);
+        TreeNode *tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        return root;
+    }
